a54f392158d49645c9c35444400568979a36d8e5
Arden Chew <achew4@jhu.edu>
2019-02-23 17:15:16 -0500

    input output load

---
+++ programming_01/hex.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/* Format of output line:
+aaaa  hh hh hh hh hh hh hh hh  cccccccc\n
+*/
+
+/* You may want to use this */
+char hex2asc[17]="0123456789ABCDEF";
+
+int main()
+{
+  return(0);
+}
