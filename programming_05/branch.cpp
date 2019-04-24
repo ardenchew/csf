737db7e82ef20cdd1e482d549eb31c70404c8bf5
Arden Chew <achew4@jhu.edu>
2019-04-24 19:34:47 -0400

    fixed the deletion woohoo

--- programming_05/branch.cpp
+++ programming_05/branch.cpp
@@ -4,71 +4,95 @@
 void STA(unsigned long int counts[3], unsigned long long int ba,
          unsigned long long int ta, bool T) {
 
-    if (ta < ba) {
-        if (T) 
-            counts[0]++;
-        else
-            counts[1]++;
-    } else {
-        if (T)
-            counts[1]++;
-        else
-            counts[0]++;
-    }
-}
-
-void BAH(unsigned long int counts[3], unsigned long long int ba,
-         bool T, int hash[1024]) {
-
-    if (hash[ba & 1023]) {
-        if (T) {
-            counts[0]++;
-        } else {
-            counts[1]++;
-            hash[ba & 1023] = 0;
-        }
-    } else {
-        if (T) {
-            counts[1]++;
-            hash[ba & 1023] = 1;
-        } else {
-            counts[0]++;
-        }
-    }
-
+    ((ta < ba) == T) ? counts[0]++ : counts[1]++;
 
 }
+
+void AH(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         bool T, int hash[1024], unsigned long long int bdict[1024], unsigned long long int tdict[1024], char type) {
+
+    unsigned int loc = (type == 'B') ? ba & 1023 : (type == 'T') ? ta & 1023 : ((ba & 31) << 5) + (ta & 31);
+    if ((bdict[loc] != ba) || (tdict[loc] != ta)) { counts[2]++; }
+    else { (hash[loc] == T) ? counts[0]++ : counts[1]++; }
+    bdict[loc] = ba;
+    tdict[loc] = ta;
+    hash[loc] = T;
+
+}
+
+void COL(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         bool T, int hash[1024], unsigned long long int bdict[1024], unsigned long long int tdict[1024]) {
+
+    unsigned int loc = ba & 1023;
+    if ((bdict[loc] != ba) || (tdict[loc] != ta)) { STA(counts,ba,ta,T); }
+    else { (hash[loc] == T) ? counts[0]++ : counts[1]++; }
+    bdict[loc] = ba;
+    tdict[loc] = ta;
+    hash[loc] = T;
+
+}
+
+void SAT(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         bool T, int hash[1024], unsigned long long int bdict[1024], unsigned long long int tdict[1024]) {
+
+    unsigned int loc = ba & 1023;
+    if ((bdict[loc] != ba) || (tdict[loc] != ta)) { counts[2]++; }
+    else { ((hash[loc] >= 2) == T) ? counts[0]++ : counts[1]++; }
+    bdict[loc] = ba;
+    tdict[loc] = ta;
+    if (T) { hash[loc] += (hash[loc] == 3) ? 0 : 1; }
+    else { hash[loc] -= (hash[loc] == 0) ? 0 : 1; }
+
+}
+
+void TWO(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         bool T, int hash[1024], int pred[32],
+         unsigned long long int bdict[1024], unsigned long long int tdict[1024]) {
+
+    unsigned int loc = ba & 1023;
+    if ((bdict[loc] != ba) || (tdict[loc] != ta)) { counts[2]++; }
+    else { (pred[hash[loc]] == T) ? counts[0]++ : counts[1]++; }
+    bdict[loc] = ba;
+    tdict[loc] = ta;
+    pred[hash[loc]] = T;
+    hash[loc] = (hash[loc] << 1) & 31;
+    if (T) { hash[loc]++; };
+}
+
 
 int main(int argc, char* argv[]) {
 
-    if (argc > 1)
-         return 1;
+    if (argc > 1) { return 1; }
+    unsigned long int counts[7][3] = {0};
 
-    //initialize counts
-    unsigned long int counts[7][3];
-    for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 3; j++) {
-            counts[i][j] = 0;
-        }
-    }
+    int bah_hash[1024] = {0};
+    unsigned long long int bah_bdict[1024] = {0}, bah_tdict[1024] = {0};
+    int tah_hash[1024] = {0};
+    unsigned long long int tah_bdict[1024] = {0}, tah_tdict[1024] = {0};
+    int bta_hash[1024] = {0};
+    unsigned long long int bta_bdict[1024] = {0}, bta_tdict[1024] = {0};
+    int col_hash[1024] = {0};
+    unsigned long long int col_bdict[1024] = {0}, col_tdict[1024] = {0}; 
+    int sat_hash[1024] = {0};
+    unsigned long long int sat_bdict[1024] = {0}, sat_tdict[1024] = {0}; 
+    int two_hash[1024] = {0};
+    int two_pred[32] = {0};
+    unsigned long long int two_bdict[1024] = {0}, two_tdict[1024] = {0};   
 
-    int bah_hash[1024];
-    for (int i = 0; i < 1024; i++) {
-        bah_hash[i] = 0;
-    }
-
-    unsigned long long int ba;
-    unsigned long long int ta;
+    unsigned long long int ba, ta;
     char flag;
 
     while (scanf("%llx %llx %c",&ba,&ta,&flag) == 3) {
         bool T = (flag == 84);
 
         STA(counts[0],ba,ta,T);
-        BAH(counts[1],ba,T,bah_hash);
-
+        AH(counts[1],ba,ta,T,bah_hash,bah_bdict,bah_tdict,'B');
+        AH(counts[2],ba,ta,T,tah_hash,tah_bdict,tah_tdict,'T');
+        AH(counts[3],ba,ta,T,bta_hash,bta_bdict,bta_tdict,'A');
+        COL(counts[4],ba,ta,T,col_hash,col_bdict,col_tdict);
+        SAT(counts[5],ba,ta,T,sat_hash,sat_bdict,sat_tdict);
+        TWO(counts[6],ba,ta,T,two_hash,two_pred,two_bdict,two_tdict);
     }
-
 
     printf("STA: %20llu %20llu %20llu\n",counts[0][0],counts[0][1],counts[0][2]);
     printf("BAH: %20llu %20llu %20llu\n",counts[1][0],counts[1][1],counts[1][2]);
