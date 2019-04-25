4e1f655eacfea7d9633c79cfce9d09d4a4ef9449
Arden Chew <achew4@jhu.edu>
2019-04-24 20:44:30 -0400

    c trial

---
+++ programming_05/c/branch.h
@@ -0,0 +1,22 @@
+#ifndef BRANCH_H
+#define BRANCH_H
+
+void STA(unsigned long int counts[3], unsigned long long int ba,
+         unsigned long long int ta, int T);
+
+void AH(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         int T, int hash[1024], unsigned long long int bdict[1024], unsigned long long int tdict[1024], char type);
+
+void COL(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         int T, int hash[1024], unsigned long long int bdict[1024], unsigned long long int tdict[1024]);
+
+void SAT(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         int T, int hash[1024], unsigned long long int bdict[1024], unsigned long long int tdict[1024]);
+
+void TWO(unsigned long int counts[3], unsigned long long int ba, unsigned long long int ta,
+         int T, int hash[1024], int pred[32],
+         unsigned long long int bdict[1024], unsigned long long int tdict[1024]);
+
+int main(int argc, char* argv[]);
+
+#endif
