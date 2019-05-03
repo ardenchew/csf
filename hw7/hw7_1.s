3f690a65c33c16ee4b5af21ff123ab90e610e104
Arden Chew <achew4@jhu.edu>
2019-05-02 20:51:55 -0400

    hw7 scripts

---
+++ hw7/hw7_1.s
@@ -0,0 +1,21 @@
+######################################################################
+# SPIM source file for homework 7, problem 1                         #
+#                                                                    #
+# In SPIM, use: load "hw7_1.s"                                       #
+# Then use the step command to step through instructions.            #
+#                                                                    #
+# The test instruction is preceeded and followed by NOPs to make it  #
+# easier to spot in the SPIM output.  The branch target is set to    #
+# next instruction so that the code will execute the same way        #
+# regardless of whether the branch is taken or not.                  #
+######################################################################
+	.text
+	.globl main
+main:
+	nop
+	blt  $s0,$s1,next	# Test instruction -- replace as needed
+next:
+	nop
+
+	jr   $ra
+
